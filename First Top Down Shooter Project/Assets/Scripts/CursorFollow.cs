4ae0956668253bce900ddea46e257272485dc7e6
Kshitijv24 <kshitijv24@gmail.com>
2022-03-23 12:57:09 +0530

    Added Custom Mouse Cursor

---
+++ First Top Down Shooter Project/Assets/Scripts/CursorFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CursorFollow : MonoBehaviour
+{
+    private void Start(){
+        Cursor.visible = false;
+    }
+
+    private void Update(){
+        transform.position = Input.mousePosition;
+    }
+}
